1b96a66fa24f6bb9561938077daf906be82ba807
GrandCatsWineGrapes <grandcatswinegrapes@gmail.com>
2020-10-01 18:45:54 +0300

    removed ./dist from repo

--- dist/serverInfo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serverInfo = void 0;
-var fs = require('fs');
-var path = require('path');
-var rawInfo = fs.readFileSync(path.join(__dirname, '../config.json'));
-exports.serverInfo = JSON.parse(rawInfo);
-//# sourceMappingURL=serverInfo.js.map
